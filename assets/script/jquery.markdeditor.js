63dac1645281136bd0a59bc3d153671874754583
liyiwen <liyiwen@huoyunren.com>
2015-05-13 22:05:48 +0800

    1

--- assets/script/jquery.markdeditor.js
+++ assets/script/jquery.markdeditor.js
@@ -11,26 +11,47 @@
 		markdeditor: function(settings) {
 			var defaultSetting = {
 				wrapper: this,		//自定义容器，默认就是this
-				autosize: true,
-				panelContainer: "#editor-panel"
+				autoSize: true,
+				mode: 1,			//0=简单模式，1=高级模式
+				lineHeight: 20,
+				headerContainer: ".header",
+				panelContainer: "#editor-panel",
+				groupButtonContainer: "#editor-button-group",
+				aceContainer: "editor-textarea"
 			};
 			settings = $.extend({}, defaultSetting, settings);
 
+			var text;
+			var editor;
+
+			if(typeof settings.headerContainer == "string") {
+				settings.headerContainer = $(settings.headerContainer);
+			}
 			if(typeof settings.panelContainer == "string") {
 				settings.panelContainer = $(settings.panelContainer);
 			}
+			if(typeof settings.groupButtonContainer == "string") {
+				settings.groupButtonContainer = $(settings.groupButtonContainer);
+			}
 
+			//自适应宽高
 			var autoSize = function() {
 				var width = $(window).width();
 				var height = $(window).height();
-				settings.panelContainer.width(width / 2);
-				settings.panelContainer.height(height - 50);
+				settings.panelContainer.width(width >> 1);
+				settings.panelContainer.height(height - settings.headerContainer.height());
+				settings.groupButtonContainer.css("left", (width >> 1) - settings.groupButtonContainer.width());
 			};
 
-			if(settings.autosize) {
+			if(settings.autoSize) {
 				window.onresize = autoSize;
 				autoSize();
 			}
+
+			editor = ace.edit(settings.aceContainer);
+			editor.setTheme("ace/theme/xcode");
+			editor.session.setMode("ace/mode/markdown");
+			editor.session.setUseWrapMode(true);
 		}
 	});
 	
